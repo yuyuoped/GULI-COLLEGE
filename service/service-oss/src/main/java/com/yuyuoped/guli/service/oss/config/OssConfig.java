9f7b3fe5ff8049983bcd8cc6a4238d20f8a1747b
yuyuoped <yuyuoped999@gmail.com>
2022-06-15 11:08:08 +0800

    将阿里云accesskey配置放到本地配置文件中

--- service/service-oss/src/main/java/com/yuyuoped/guli/service/oss/config/OssConfig.java
+++ service/service-oss/src/main/java/com/yuyuoped/guli/service/oss/config/OssConfig.java
@@ -1,20 +1,26 @@
 package com.yuyuoped.guli.service.oss.config;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author yuyuoped
  * @since 2022/6/14
  */
 @Configuration
-@ConfigurationProperties(prefix = "aliyun.oss")
+@PropertySource(value = "classpath:application-local.properties")
 @Data
 public class OssConfig {
+    @Value("${aliyun.oss.schema}")
     String schema;
+    @Value("${aliyun.oss.endpoint}")
     String endpoint;
+    @Value("${aliyun.oss.accessKeyId}")
     String accessKeyId;
+    @Value("${aliyun.oss.accessKeySecret}")
     String accessKeySecret;
+    @Value("${aliyun.oss.bucketName}")
     String bucketName;
 }
